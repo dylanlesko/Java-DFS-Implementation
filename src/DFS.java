6803eace666441f59b3dbe99d15c9b05d0ac2ae2
Dylan Lesko <dylan@lesko.io>
2015-10-26 18:16:32 -0400

    Create DFS.java

---
+++ src/DFS.java
@@ -0,0 +1,73 @@
+import java.util.Random;
+
+public class DFS
+{
+	private int[][] grid;
+	private int[] visited;
+	private int stationProb;
+	private int n;
+	private int charge;
+
+	public DFS(int vert, double charProb)
+	{
+		stationProb = (int)(charProb*100);
+		n = vert;
+		grid = new int[n][n];
+		visited = new int[n];
+		charge = 10;
+		buildGraph();
+	}
+	public void buildGraph()
+	{
+		int randomNum;
+		Random rand = new Random();
+
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				randomNum = rand.nextInt(100) + 1;
+				if(stationProb > randomNum)
+				{
+					grid[i][j] = 1;
+				}
+				else
+				{
+					grid[i][j] = 0;
+				}
+			}
+		}
+	}
+
+	public void printGraph()
+	{
+		for(int i = 0; i < n; i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				System.out.print( "\t" + grid[ i ][j]);
+			}
+			System.out.print("\n");
+		}
+	}
+
+	public void DFS(int i)
+	{
+		int j;
+		//printf("\n%d",i);
+		System.out.print( i );
+		visited[i]=1;
+
+		for(j=0;j<n;j++)
+		{
+			if ( visited[ j ] == 0  )
+			{
+				if( charge > 0 || grid[ i ][ j ] == 1)
+				{
+					charge--;
+					DFS( j );
+				}
+			}
+		}
+	}
+}
