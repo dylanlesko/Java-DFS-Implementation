8d14cc91ebc8393594b87a98168958c3c900ce75
Savataged <dylanlesko22@gmail.com>
2015-10-26 23:31:52 -0400

    finding paths properly

--- src/DFS.java
+++ src/DFS.java
@@ -5,14 +5,12 @@ public class DFS
 	private int[][] grid;
 	private int stationProb;
 	private int n;
-	private int charge;
 
-	public DFS(int vert, int charProb)
+    public DFS(int vert, int charProb)
 	{
 		n = vert;
         stationProb = charProb;
 		grid = new int[n][n];
-		charge = 10;
 		buildGraph();
 	}
 	public void buildGraph()
@@ -51,49 +49,71 @@ public class DFS
 
     public int FindEndPath()
     {
-        DepthSearch( 0, 0, 0, 0, "" );
+        search(0,0,"down", "", 10);
         return 1;
     }
 
-	public int DepthSearch(int up, int down, int left, int right, String path)
-	{
-        String currentNode = "[" + down + "," + right + "] charge: " + charge +"\n";
+    public int search(int x, int y, String step, String path, int curCharge)
+    {
+        String currentNode = "[" + x + "," + y + "] charge: " + (curCharge) +"\n";
         path = path + currentNode;
 
-        /* Check if at the [n,n] node */
-        if( down==(n-1) && right == (n-1) )
+        int newCharge = curCharge;
+
+        if(x==(n-1) && y==(n-1))
         {
-            System.out.println(path);
+            System.out.print(path);
             return 1;
         }
 
-        /* Check if enough charge to proceed, or if the current node has a station */
-        if(charge <= 0 && grid[down][right] == 0)
+        if(x >= 0 && y >= 0 && x < n && y < n)
         {
-            return 0;
-        }
-        if(grid[down][right] == 1)
-        {
-            charge = 10;
-        }
+            if(grid[x][y] == 1)
+            {
+                newCharge = 10;
+            }
+            else
+            {
+                newCharge--;
+            }
 
-        if(down == (n-1))
-        {
-            charge--;
-            DepthSearch( up, down, left, right+1, path );
-        }
-        else if(right == (n-1))
-        {
-            charge--;
-            DepthSearch( up, down+1, left, right, path );
-        }
-        else
-        {
-            charge--;
-            DepthSearch(0, down+1,0, right, path);
-            DepthSearch( 0, down, 0, right+1, path );
+            if(newCharge > 0)
+            {
+                if(step != "up")
+                {
+                    if (search(x+1, y, "down", path, newCharge) == 1)
+                    {
+                       return 1;
+                    }
+                }
+                if(step != "left")
+                {
+                    if (search(x, y+1, "right", path, newCharge) == 1)
+                    {
+                        return 1;
+                    }
+                }
+                if(step != "down")
+                {
+                    if (search(x-1, y, "up", path, newCharge) == 1)
+                    {
+                        return 1;
+                    }
+                }
 
+                if(step != "right")
+                {
+                    if (search(x, y-1, "left", path, newCharge) == 1)
+                    {
+                        return 1;
+                    }
+                }
+
+                return 0;
+            }
         }
-		return 1;
-	}
+        return 0;
+    }
+
+
 }
--- src/algo.java
+++ src/algo.java
@@ -4,19 +4,18 @@ public class algo
 {
     public static void main(String[] args)
 	{
-        /*
+
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Please Enter n: ");
         int n = keyboard.nextInt();
         System.out.println("Please percentage that a spot has a charge statiom (0 - 100): ");
         int percent = keyboard.nextInt();
-        */
 
-        DFS searchTest = new DFS(10, 5);
-        //DFS searchTest = new DFS(n, percent);
+
+       // DFS searchTest = new DFS(10, 5);
+        DFS searchTest = new DFS(n, percent);
 
         searchTest.printGraph();
-
         searchTest.FindEndPath();
 	}
 }
